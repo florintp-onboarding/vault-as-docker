2a93e3dc2e5d0fee4f86047c45f86baedb5ef164
Florin Tiucra-Popa <106736683+FlorinTP@users.noreply.github.com>
2024-04-12 15:10:42 +0200

    Change the TIMEDEMO and the display

--- create_and_run_container.sh
+++ create_and_run_container.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-
 # Default port exposed from container is 8200
-# Default license is collected from the license file ../vault_license.hlic
+export VAULT_ADDR="http://localhost:8200"
+VAULT_LICENSE=$(cat vault_license.hclic 2>/dev/null)
 
+# Default license is collected from the license file ./vault_license.hclic
 # The demo will be kept alive for a number of seconds as per variable TIMEDEMO
-# Default value for TIMEDEMO is 100 seconds. TIMEDEMO=100
-export TIMEDEMO=100
+# Default value for TIMEDEMO is 3600 seconds. TIMEDEMO=3600
+export TIMEDEMO=${TIMEDEMO:-3600}
 
-export VAULT_ADDR=http://localhost:8200
-VAULT_LICENSE=$(cat ../vault_license.hlic)
-echo -ne "\nLicense is:\$VAULT_LICENSE\n"
+# DEBUG information is printed if there is a terminal and if the value is greater than ZERO.
+export DEBUG=${DEBUG:-0}
+
+export VAULT_LICENSE=$(cat vault_license.hclic 2>/dev/null)
+if [ "Z$VAULT_LICENSE" == "Z" ] ; then
+        printf '\nNo valid license loaded!\n'
+        exit 1
+fi
+
+[ -t ] && [ $DEBUG -gt 0 ] && echo -ne "\nLicense used:$VAULT_LICENSE\n"
 
 export VAULT_TOKEN=$(echo "my_temporary_token" |base64 )
-echo "Token used: $VAULT_TOKEN"
+printf '\nToken used:%sa' "$VAULT_TOKEN"
 
 docker run \
 	--cap-add=IPC_LOCK \
@@ -26,10 +34,10 @@ docker run \
 	--detach \
 	hashicorp/vault-enterprise
 
-echo "The docker container to work on is:"
+printf '\nThe docker container to work on is:'
 docker ps -q -f  "ancestor=hashicorp/vault-enterprise" -f "status=running" --format "{{.ID}}\t{{.Names}}"|head -n1
 
-echo "Logs from the container:"
+printf '\nLogs from the container:'
 running_container=$(docker ps -q -f  "ancestor=hashicorp/vault-enterprise" -f "status=running" --format "{{.Names}}"|head -n1)
 
 # Tail with follow on the logs of the new container...
@@ -37,10 +45,13 @@ docker logs --follow $running_container &
 export temp_pid=$!
 sleep 1
 
-echo "Sleeping $TIMEDEMO seconds"
+printf '\nSleeping %si seconds.' "$TIMEDEMO"
 sleep $TIMEDEMO
 
 [ $temp_pid -gt 100 ] && kill -STOP $temp_pid
 [ $temp_pid -gt 100 ] && [ $(ps -fe|grep -v grep |grep $temp_pid|wc -l|tail -n1 2>/dev/null) -ge 1 ] && kill -TERM  $temp_pid
 
 docker rm --force $(docker ps -q -a -f  "ancestor=hashicorp/vault-enterprise"  --format "{{.Names}}"|head -n1)
+
+print '\nDone'
+
